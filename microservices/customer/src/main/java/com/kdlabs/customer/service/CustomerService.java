77c1b060d9baf5a892962eb8e857bc3b392b0c1c
kunal-das <kd.kunaldas92@gmail.com>
2022-02-14 08:43:43 +0530

    Commiting changes of Section 2 - Customer microservice with postgres customer db

--- microservices/customer/src/main/java/com/kdlabs/customer/service/CustomerService.java
+++ microservices/customer/src/main/java/com/kdlabs/customer/service/CustomerService.java
@@ -2,10 +2,15 @@ package com.kdlabs.customer.service;
 
 import com.kdlabs.customer.dto.CustomerRequest;
 import com.kdlabs.customer.entity.Customer;
+import com.kdlabs.customer.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
+
+    private CustomerRepository customerRepository;
 
     public void registerCustomer(CustomerRequest request) {
         Customer newCustomer = Customer.builder()
@@ -13,6 +18,8 @@ public class CustomerService {
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .build();
+
+        customerRepository.save(newCustomer);
 
         // TODO : check if email is valid and not taken
 
