e35370d0a187cbb92ad464cbded4a590b63a682e
kunal-das <kd.kunaldas92@gmail.com>
2022-02-19 21:46:50 +0530

    Section 8 - Api Gateway - Implemented Api Gateway in arch

--- microservices/customer/src/main/java/com/kdlabs/customer/controller/CustomerController.java
+++ microservices/customer/src/main/java/com/kdlabs/customer/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.kdlabs.customer.service.CustomerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class CustomerController {
 
-    private CustomerService customerService;
+    private final CustomerService customerService;
+
+    private final Environment env;
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.OK)
     public void registerCustomer(@RequestBody CustomerRequest request) {
 
-        log.info("New customer {}", request);
+        log.info("New customer {}, server.port: {}", request, env.getProperty("server.port"));
         customerService.registerCustomer(request);
     }
 }
