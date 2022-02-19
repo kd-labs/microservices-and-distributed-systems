25770033887393f3e2bf10577e6d4b9151163266
kunal-das <kd.kunaldas92@gmail.com>
2022-02-19 23:41:08 +0530

    Section 9 - Asynchronous Communication - configured amqp project library defining rabbit mq configurations - added amqp dependency to customer and notification microservice - defined the location of rabbitmq container in application of customer and notification service

--- microservices/customer/src/main/java/com/kdlabs/customer/CustomerApplication.java
+++ microservices/customer/src/main/java/com/kdlabs/customer/CustomerApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.kdlabs.customer",
+                "com.kdlabs.amqp"
+        }
+)
 @EnableEurekaClient
 public class CustomerApplication {
 
--- microservices/notification/src/main/java/com/kdlabs/notification/NotificationApplication.java
+++ microservices/notification/src/main/java/com/kdlabs/notification/NotificationApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.kdlabs.notification",
+                "com.kdlabs.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
 
